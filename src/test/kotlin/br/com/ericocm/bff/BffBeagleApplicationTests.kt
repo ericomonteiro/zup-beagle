b71f05a73917aa55f7475bb2e086e8ca76f415fe
Érico Monteiro <erico.cintra@gmail.com>
2020-06-24 09:34:04 -0300

    Create project

---
+++ src/test/kotlin/br/com/ericocm/bff/BffBeagleApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ericocm.bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BffBeagleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
