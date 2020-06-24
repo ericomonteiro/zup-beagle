d50fdf4f9be1871231ae5d569cd1080a1013b9fd
johnmarcus015 <johnmarcus015@gmail.com>
2020-06-24 15:30:27 -0300

    Implementado beagle core

---
+++ Beagle1/app/src/main/java/br/com/ztx/beagle_1/AppBeagleConfig.kt
@@ -0,0 +1,31 @@
+package br.com.ztx.beagle_1
+
+import br.com.ztx.beagle_1.AppBeagleConfig.Constants.DEFAULT_CACHE_ENABLED
+import br.com.ztx.beagle_1.AppBeagleConfig.Constants.DEFAULT_MAX_AGE
+import br.com.ztx.beagle_1.AppBeagleConfig.Constants.DEFAULT_MEMORY_MAX_CAPACITY
+import br.com.zup.beagle.annotation.BeagleComponent
+import br.com.zup.beagle.setup.BeagleConfig
+import br.com.zup.beagle.setup.Cache
+import br.com.zup.beagle.setup.Environment
+
+@BeagleComponent
+class AppBeagleConfig : BeagleConfig {
+
+    //TODO Trocar mock url quando houver endpoint
+    override val baseUrl: String get() = "https://myapp.server.com/"
+
+    override val environment: Environment get() = Environment.DEBUG
+
+    override val cache: Cache = Cache(
+        enabled = DEFAULT_CACHE_ENABLED,
+        maxAge = DEFAULT_MAX_AGE,
+        memoryMaximumCapacity = DEFAULT_MEMORY_MAX_CAPACITY
+    )
+
+    private object Constants {
+        const val DEFAULT_CACHE_ENABLED = true
+        const val DEFAULT_MAX_AGE = 300L
+        const val DEFAULT_MEMORY_MAX_CAPACITY = 15
+    }
+
+}
---
+++ Beagle1/app/src/main/java/br/com/ztx/beagle_1/components/actions/CustomAction.kt
@@ -0,0 +1,13 @@
+package br.com.ztx.beagle_1.components.actions
+
+import android.content.Context
+import android.widget.Toast
+import br.com.zup.beagle.action.Action
+
+class CustomAction: Action {
+
+    constructor(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+}
