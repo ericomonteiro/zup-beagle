a87589dd7ed44bea36aaee5abd7719b4eed06213
Érico Monteiro <erico.cintra@gmail.com>
2020-06-24 13:42:57 -0300

    Http status on mocked return

---
+++ src/main/kotlin/br/com/ericocm/bff/MockContent.kt
@@ -0,0 +1,11 @@
+package br.com.ericocm.bff
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class MockContent (
+        @JsonProperty("httpStatus")
+        val httpStatus: Int,
+
+        @JsonProperty("content")
+        val content: Any?
+)
--- src/main/kotlin/br/com/ericocm/bff/controller/MockController.kt
+++ src/main/kotlin/br/com/ericocm/bff/controller/MockController.kt
@@ -1,8 +1,11 @@
 package br.com.ericocm.bff.controller
 
+import br.com.ericocm.bff.MockContent
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.swagger.models.HttpMethod
 import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,29 +15,33 @@ class MockController {
 
     @GetMapping("{folder}/{file}")
     fun getGenericJson(@PathVariable folder: String, @PathVariable file : String) : Any? {
-        return fileJSONToObject(folder, file, HttpMethod.GET)
+        val mockContent = fileJSONToObject(folder, file, HttpMethod.GET)
+        return ResponseEntity.status(mockContent.httpStatus).body(mockContent.content)
     }
 
     @PostMapping("{folder}/{file}")
     fun postGenericJson(@PathVariable folder: String, @PathVariable file : String) : Any? {
-        return fileJSONToObject(folder, file, HttpMethod.POST)
+        val mockContent = fileJSONToObject(folder, file, HttpMethod.POST)
+        return ResponseEntity.status(mockContent.httpStatus).body(mockContent.content)
     }
 
     @PutMapping("{folder}/{file}")
     fun putGenericJson(@PathVariable folder: String, @PathVariable file : String) : Any? {
-        return fileJSONToObject(folder, file, HttpMethod.PUT)
+        val mockContent = fileJSONToObject(folder, file, HttpMethod.PUT)
+        return ResponseEntity.status(mockContent.httpStatus).body(mockContent.content)
     }
 
     @DeleteMapping("{folder}/{file}")
-    fun deleteGenericJson(@PathVariable folder: String, @PathVariable file : String) : Any? {
-        return fileJSONToObject(folder, file, HttpMethod.DELETE)
+    fun deleteGenericJson(@PathVariable folder: String, @PathVariable file : String) : ResponseEntity<Any?> {
+        val mockContent = fileJSONToObject(folder, file, HttpMethod.DELETE)
+        return ResponseEntity.status(mockContent.httpStatus).body(mockContent.content)
     }
 
-    private fun fileJSONToObject(folder: String,file : String, httpMethod: HttpMethod) : Any? {
+    private fun fileJSONToObject(folder: String,file : String, httpMethod: HttpMethod) : MockContent {
         val fullPathName = "$staticContentPath/$folder/$httpMethod/$file.json"
         println(fullPathName)
         val resource = ClassPathResource(fullPathName)
-        return ObjectMapper().readValue(resource.inputStream, Any::class.java)
+        return ObjectMapper().readValue(resource.inputStream, MockContent::class.java)
     }
 
 
